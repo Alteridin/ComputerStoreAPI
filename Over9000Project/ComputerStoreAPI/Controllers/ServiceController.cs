595b9bc9ab1e45abb9c9e015b91a314713adb0e1
blair909 <61510475+blair909@users.noreply.github.com>
2020-04-28 13:15:00 -0500

    Hopefully Last Derp

---
+++ Over9000Project/ComputerStoreAPI/Controllers/ServiceController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNet.Identity;
+using Over9000.Models.ServiceModel;
+using Over9000.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ComputerStoreAPI.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        private ServiceServices CreateServiceService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var serviceService = new ServiceServices(userId);
+            return serviceService;
+        }
+        public IHttpActionResult Get()
+        {
+            ServiceServices serviceService = CreateServiceService();
+            var services = serviceService.GetServices();
+            return Ok(services);
+        }
+        public IHttpActionResult Post(ServiceCreate servicez)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateServiceService();
+            if (!service.ServiceCreate(servicez))
+                return InternalServerError();
+            return Ok();
+        }
+        public IHttpActionResult Put(ServiceEdit servicez)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateServiceService();
+            if (!service.UpdateService(servicez))
+                return InternalServerError();
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateServiceService();
+            if (!service.DeleteService(id))
+                return InternalServerError();
+            return Ok();
+        }
+    }
+}
---
+++ Over9000Project/ComputerStoreAPI/Controllers/ServiceReviewController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNet.Identity;
+using Over9000.Models.ServiceReviewModel;
+using Over9000.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ComputerStoreAPI.Controllers
+{
+    public class ServiceReviewController : ApiController
+    {
+        private ServiceReviewServices CreateServiceReviewService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var srService = new ServiceReviewServices(userId);
+            return srService;
+        }
+        public IHttpActionResult Get()
+        {
+            ServiceReviewServices srService = CreateServiceReviewService();
+            var services = srService.GetServiceReviews();
+            return Ok(services);
+        }
+        public IHttpActionResult Post(ServiceReviewCreate sr)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateServiceReviewService();
+            if (!service.ServiceReviewCreate(sr))
+                return InternalServerError();
+            return Ok();
+        }
+        public IHttpActionResult Put(ServiceReviewEdit sr)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateServiceReviewService();
+            if (!service.UpdateServiceReview(sr))
+                return InternalServerError();
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateServiceReviewService();
+            if (!service.DeleteServiceReview(id))
+                return InternalServerError();
+            return Ok();
+        }
+    }
+}
